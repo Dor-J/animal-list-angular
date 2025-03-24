76f0741c3030484f0b21c2838b8eebf73bcb0cb4
Dor-J <Mr.DorJosef@gmail.com>
2025-03-24 16:03:32 +0200

    Add animalInfos data and interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { AnimalInfos } from './models/animalInfos.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'animal-list';
+  title = 'animal-list-angular';
+
+  animalInfos: AnimalInfos[] = [
+    { type: 'Malayan Tiger', count: 787 },
+    { type: 'Mountain Gorilla', count: 212 },
+    { type: 'Fin Whale', count: 28 },
+  ];
 }
